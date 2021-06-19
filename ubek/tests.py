0faa99a1aaea291cd5de76d5078b402bba952dcc
kami2 <kami.krasowski.uk@gmail.com>
2021-06-19 16:47:37 +0100

    Login test

--- ubek/tests.py
+++ ubek/tests.py
@@ -1,15 +1,17 @@
 from django.test import TestCase
 from .models import Profile, User
+from django.contrib.auth import get_user_model
+
 
 # Create your tests here.
-
+User = get_user_model()
 
 class URLTest(TestCase):
-    def test_homepage(self):
+    def test_is_homepage_redirect(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 302)
 
-    def test_loginpage(self):
+    def test_is_loginpage_response(self):
         response = self.client.get('/login/')
         self.assertEqual(response.status_code, 200)
 
@@ -19,4 +21,22 @@ class ProfileModelTest(TestCase):
         profile = User.objects.create_user('test', 'test@test.com', 'testtesttest')
         self.assertEqual(str(profile), 'test')
 
+
+class LoginUserTest(TestCase):
+
+    def setUp(self):
+        test_user = User(username='test', email='test@test.com')
+        test_user.set_password('testtesttest')
+        test_user.save()
+        self.test_user_pw = test_user.password
+
+
+    def test_login_user_view(self):
+        login_url = self.client.get('/login/')
+        data = {"username" : "test", "password" : self.test_user_pw}
+        response = self.client.post(login_url, data, follow=True)
+        status_code = response.status_code
+        redirect_path = response.request.get('PATH_INFO')
+        #self.assertEqual(redirect_path, '/')
+        self.assertEqual(status_code, 200)
 
