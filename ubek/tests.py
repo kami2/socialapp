4c4af8eb1625a53e8925703bad90a76aff9fc25a
kami2 <kami.krasowski.uk@gmail.com>
2021-06-19 13:31:17 +0100

    Add tests

--- ubek/tests.py
+++ ubek/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Profile, User
 
 # Create your tests here.
+
+
+class URLTest(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_loginpage(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+
+
+class ProfileModelTest(TestCase):
+    def test_profile_str_return(self):
+        profile = User.objects.create_user('test', 'test@test.com', 'testtesttest')
+        self.assertEqual(str(profile), 'test')
+
+
