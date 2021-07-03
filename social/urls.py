4cd3e96d89f2f2ede38950053f0002eec7b21833
kami2 <kami.krasowski.uk@gmail.com>
2021-07-03 21:17:42 +0100

    Add custom 404 page

--- social/urls.py
+++ social/urls.py
@@ -16,6 +16,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import handler404
+from ubek import views
 
 
 urlpatterns = [
@@ -24,3 +26,5 @@ urlpatterns = [
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'ubek.views.error_404'
